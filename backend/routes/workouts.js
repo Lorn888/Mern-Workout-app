37dc0298a04baa27f96572006213a9857eb82676
Patryk <patrykokpara@hotmail.com>
2023-03-14 17:49:48 +0000

    Update

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -8,4 +8,11 @@ router.get('/', (req, res) => {
     res.json({mssg: 'GET all workouts'})
 })
 
+//Get a single workout
+router.get('/:id', (req, res) =>{
+    res.json({mssg: 'GET a single workout'})
+})
+
+//
+
 module.exports = router
