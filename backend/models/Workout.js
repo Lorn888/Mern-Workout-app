b5e34345059c08a0fded8c7363a820740fc03c78
Patryk <patrykokpara@hotmail.com>
2023-03-14 18:57:10 +0000

    Added schema (structure of the doc)

---
+++ backend/models/Workout.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const workoutSchema = new Schema ({
+
+    //First argument describes what the document should look like
+    title: {
+        type: String,
+        required : true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    load: {
+        type: Number,
+        required: true    
+    }
+}, 
+//Second adds a timestamp
+
+{ timestamps: true }) 
