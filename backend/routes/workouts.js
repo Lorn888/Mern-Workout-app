2b89b9c845d82238a3f4d7bd6ae504ff547a998e
Patryk <patrykokpara@hotmail.com>
2023-03-14 17:53:09 +0000

    Added delete and update workout

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -13,6 +13,19 @@ router.get('/:id', (req, res) =>{
     res.json({mssg: 'GET a single workout'})
 })
 
-//
+//POST a new workout
+router.post('/', (req, res) => {
+    res.json({mssg:'POST new workout'})
+})
+
+//DELETE a workout
+router.delete('/:id', (req, res) => {
+    res.json({mssg:'DELETE a  workout'})
+})
+
+//UPDATE a workout
+router.patch('/:id', (req, res) => {
+    res.json({mssg:'UPDATE a workout'})
+})
 
 module.exports = router
