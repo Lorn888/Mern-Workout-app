5dea19ee6a48b41c4ee2c64bf95d5d438f3d30ac
Patryk <patrykokpara@hotmail.com>
2023-03-14 17:34:01 +0000

    Added middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,14 @@ require('dotenv').config()
 //express app
 const app = express()
 
+//middleware
+app.use((req,res,next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+
+//routes
 app.get('/', (req, res) =>{
     res.json({mssg: 'Welcome to the app'})
 })
