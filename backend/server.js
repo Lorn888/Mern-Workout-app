3d6aa9421ff8e3cb9f4fe6a7cfc9ae1d94b17b1b
Patryk <patrykokpara@hotmail.com>
2023-03-14 17:12:47 +0000

    Added a route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const express = require ('express')
 //express app
 const app = express()
 
+app.get('/', (req, res) =>{
+    res.json({mssg: 'Welcome to the app'})
+})
+
 
 //listen for request
 app.listen(7000, () =>{
